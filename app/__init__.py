892372b0e7e4a3673b84acf6b39ffc44598dd0fd
Cadell Griffith <cadellgriffith@Cadells-MacBook-Air.local>
2022-08-19 17:35:00 +0100

    postresql modifications

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 #app configuration
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://akjecvzsaeywvv:15109f1557aa7a0f22eb198f9d6cda41a62b48f3514d95dadb2b8136364ed5cf@ec2-44-205-63-142.compute-1.amazonaws.com:5432/d52brv8dghaq0t'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://akjecvzsaeywvv:15109f1557aa7a0f22eb198f9d6cda41a62b48f3514d95dadb2b8136364ed5cf@ec2-44-205-63-142.compute-1.amazonaws.com:5432/d52brv8dghaq0t'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
