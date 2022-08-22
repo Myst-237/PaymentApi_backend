6dcab5ed93ab5c6cb1109c04e24edffd0427b45a
Cadell Griffith <cadellgriffith@Cadells-MacBook-Air.local>
2022-08-22 07:42:00 +0100

    configuration for heroku redis premium

--- worker.py
+++ worker.py
@@ -1,13 +1,17 @@
 import os
-
+from urllib.parse import urlparse
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+#redis congiguration on production with redis premium 0 add-on
+url = urlparse(os.environ.get("REDIS_URL"))
+conn = redis.Redis(host=url.hostname, port=url.port, username=url.username, password=url.password, ssl=True, ssl_cert_reqs=None)
 
-conn = redis.from_url(redis_url)
+#redis configuration on dev or heroku hobby add on
+# redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+# conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
